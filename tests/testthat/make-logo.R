c26ccf74ff76dbf1c69b8e369e41c72f0192cec6
Thomas Gredig <thomas.gredig@csulb.edu>
2024-10-20 13:00:15 -0700

    add the logo back in

--- tests/testthat/make-logo.R
+++ tests/testthat/make-logo.R
@@ -2,7 +2,7 @@
 library(nanoAFMr)
 library(ggplot2)
 da = AFM.import(AFM.getSampleImages(type='ibw'))
-plot(da, graphType=2, trimPeaks = 0.02)
+plot(da, graphType=3, trimPeaks = 0.02) + theme(plot.background = element_rect(fill = "transparent", color = NA))
 ggsave('man/figures/logo2.png', width=3.2,height=3.2)
 
 library(hexSticker) # Create Hexagon Sticker in R
@@ -14,15 +14,15 @@ font_add_google("Inconsolata", "incon")
 sticker(
   # Subplot (image)
   subplot = "man/figures/logo2.png",       # Image name
-  s_y = 1.3,                          # Position of the sub plot (y)
-  s_x = 1,                       # Position of the sub plot (x)
-  s_width = 0.4,                   # Width of the sub plot
-  s_height=0.4,                    # Height of the sub plot
+  s_y = 1.1,                          # Position of the sub plot (y)
+  s_x = 0.97,                       # Position of the sub plot (x)
+  s_width = 0.6,                   # Width of the sub plot
+  s_height=0.6,                    # Height of the sub plot
   # Font
   package = "nanoAFMr",            # Package name (will be printed on the sticker)
   p_size = 64,                       # Font size of the text
-  p_y = 0.6,                        # Position of the font (y)
-  p_x=0.85,                         # Position of the font (x)
+  p_y = 0.43,                        # Position of the font (y)
+  p_x=1.05,                         # Position of the font (x)
   p_family = "incon",               # Defines font
   # Spotlight
   spotlight = TRUE,                 # Enables spotlight
